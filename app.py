6b0824958c0cde991932deb2e33c96e64ca00c25
dhanushDev <bmdhanush05@gmail.com>
2022-08-27 22:47:58 +0530

    trying mail functionality

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
-from flask import Flask, request, jsonify
-from util import fetch
+from flask import Flask, request, jsonify, render_template
+from util import fetch, create
 from flask_cors import CORS, cross_origin
-
+from flask_mail import Mail, Message
+import socket
+hostname=socket.gethostname()
+IPAddr=socket.gethostbyname(hostname)
+print("Your Computer Name is:"+hostname)
+print("Your Computer IP Address is:"+IPAddr)
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'do.not.reply.nextstay@gmail.com'
+app.config['MAIL_PASSWORD'] = 'NextStay@gmail20'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+mail.init_app(app)
 
 @app.route('/')
 def hello_world():
-    return 'This is my first API call!'
+    return render_template('loginPage.html')
 
 
 @app.route('/login', methods=["POST"])
 def verify():
     input_json = request.get_json(force=True)
     print(input_json)
-    data =  fetch("CREDENTIALS",input_json)
+    data = fetch("CREDENTIALS", input_json)
     dictToReturn = {"authenticatedUser": False}
-    if(len(data)>0):
+    if len(data) == 1:
         dictToReturn = {"authenticatedUser": True}
     return jsonify(dictToReturn)
 
@@ -29,3 +42,18 @@ def getData():
     # input_json = request.get_json(force=True)
     dictToReturn = {'data': fetch("STAYS", None)}
     return jsonify(dictToReturn)
+
+@app.route('/manageTable', methods=["POST"])
+@cross_origin()
+def manageData():
+    # print(fetch())
+    input_json = request.get_json(force=True)
+    dictToReturn = {'data': create(input_json)}
+    return jsonify(dictToReturn)
+
+@app.route("/sendmail")
+def index():
+   msg = Message('Hello', sender = 'do.not.reply.nextstay@gmail.com', recipients = ['bmdhanush05@gmail.com'])
+   msg.body = "This is the email body"
+   mail.send(msg)
+   return "Sent"
--- util.py
+++ util.py
@@ -32,3 +32,31 @@ def fetch(dataTable, input):
     conn.commit()
     conn.close()
     return result
+
+def create(payload):
+    conn = psycopg2.connect(
+        database="d1mcchg8iig4uf", user='qgopasstjgvbtx',
+        password='936bcf6d1c2d77bb52cac3b38baef32140407294a03b629c87a473cc5b5aa7d6',
+        host='ec2-52-22-136-117.compute-1.amazonaws.com', port='5432'
+    )
+    conn.autocommit = True
+
+    # Creating a cursor object using the cursor() method
+    cursor = conn.cursor()
+
+    # Retrieving data
+    isSuccessful = 0
+    if payload != None:
+        parameters = list(payload['input'].keys())
+        values = payload['input'].values()
+        query = '''INSERT INTO {0}'''.format(payload['dataTable'])
+        query = query + ''' ('''+" ,".join(parameters) + " ) values ( '" + "' ,'".join(values) + ''' ')'''
+        print("query", query)
+        isSuccessful = cursor.execute(query)
+    # fields = [field_md[0] for field_md in cursor.description]
+    # result = [dict(zip(fields, row)) for row in cursor.fetchall()]
+    result = {"executedSuccessfully": isSuccessful}
+    print({"executedSuccessfully": isSuccessful})
+    conn.commit()
+    conn.close()
+    return result
