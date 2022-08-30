5383c42cfcc13a7e2bdc03400198d78f167240e9
dhanushDev <bmdhanush05@gmail.com>
2022-08-30 15:32:48 +0530

    Add sign on and email options

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, request, jsonify, render_template
-from util import fetch, create
+from util import fetch, create, update
 from flask_cors import CORS, cross_origin
-
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+mail = Mail(app)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'do.not.reply.nextstay@gmail.com'
+app.config['MAIL_PASSWORD'] = 'pnfkjgpwsueledoz'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
 
 
 @app.route('/')
@@ -15,21 +23,48 @@ def hello_world():
 
 @app.route('/login', methods=["POST"])
 @cross_origin()
+def login():
+    input_json = request.get_json(force=True)
+    # print(input_json)
+    data = fetch("CREDENTIALS", input_json)
+    dictToReturn = {"authenticatedUser": False}
+    if len(data) == 1 and data[0]['isVerified']:
+        dictToReturn = {"authenticatedUser": True}
+    return jsonify(dictToReturn)
+
+
+@app.route('/verify', methods=["POST"])
+@cross_origin()
 def verify():
     input_json = request.get_json(force=True)
     # print(input_json)
     data = fetch("CREDENTIALS", input_json)
     dictToReturn = {"authenticatedUser": False}
     if len(data) == 1:
-        dictToReturn = {"authenticatedUser": True}
+        payload = {"dataTable": "credentials", "input": {"isverified": True}, "query": {"email": "ddhanu371@gmail.com"}}
+        data = update(payload)
+        if data['executedSuccessfully']:
+            dictToReturn = {"authenticatedUser": True}
     return jsonify(dictToReturn)
+
+
+@app.route('/signon', methods=["POST"])
+@cross_origin()
+def signon():
+    input_json = request.get_json(force=True)
+    data = create(input_json)
+    keys = data.keys()
+    result = {"executedSuccessfully": True}
+    if 'recipients' in keys:
+        sendMail(data['recipients'], data['message'], "Verification Code")
+    else:
+        result = data
+    return jsonify(result)
 
 
 @app.route('/fetchTable', methods=["POST"])
 @cross_origin()
 def getData():
-    # print(fetch())
-    # input_json = request.get_json(force=True)
     dictToReturn = {'data': fetch("STAYS", None)}
     return jsonify(dictToReturn)
 
@@ -43,25 +78,22 @@ def manageData():
     return jsonify(dictToReturn)
 
 
-# @app.route("/sendmail")
-# def index():
-#     # msg = Message('Hello', sender='donotreplay@mailtrap.io' ,  recipients = ['bmdhanush05@gmail.com'])
-#     # msg.body = "This is the email body"
-#     # mail.send(msg)
-#     sender = 'from@fromdomain.com'
-#     receivers = ['to@todomain.com']
-#
-#     message = """From: From Person <from@fromdomain.com>
-#    To: To Person <to@todomain.com>
-#    Subject: SMTP e-mail test
-#
-#    This is a test e-mail message.
-#    """
-    #
-    # try:
-    #     smtpObj = smtplib.SMTP('localhost')
-    #     smtpObj.sendmail(sender, receivers, message)
-    #     print("Successfully sent email")
-    # except smtplib.SMTPException:
-    #     print("Error: unable to send email")
-    # return "Sent"
+@app.route('/update', methods=["POST"])
+@cross_origin()
+def updateTable():
+    # print(fetch())
+    input_json = request.get_json(force=True)
+    dictToReturn = {'data': update(input_json)}
+    return jsonify(dictToReturn)
+
+
+@app.route("/sendmail")
+def sendMail(recipients, messageBody, subject):
+    msg = Message(
+        subject,
+        sender='do.not.reply.nextstay@gmail.com',
+        recipients= recipients
+    )
+    msg.body = messageBody
+    mail.send(msg)
+    return 'Sent'
--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 import psycopg2
+from random import randint
 
 
 def fetch(dataTable, input):
@@ -33,7 +34,17 @@ def fetch(dataTable, input):
     conn.close()
     return result
 
+
 def create(payload):
+    dataTableName = payload['dataTable']
+    code = randint(100000, 999999)
+    if dataTableName == "credentials":
+        existingUser = fetch(dataTableName, {'email': payload['input']['email']})
+        if len(existingUser) > 0:
+            return {"message": "User already exist"}
+        else:
+            # payload['input']['isVerified'] = False
+            payload['input']['code'] = code
     conn = psycopg2.connect(
         database="d1mcchg8iig4uf", user='qgopasstjgvbtx',
         password='936bcf6d1c2d77bb52cac3b38baef32140407294a03b629c87a473cc5b5aa7d6',
@@ -49,12 +60,60 @@ def create(payload):
     if payload != None:
         parameters = list(payload['input'].keys())
         values = payload['input'].values()
+        delimiter = []
+        for value in values:
+            delimiter.append("%s")
         query = '''INSERT INTO {0}'''.format(payload['dataTable'])
-        query = query + ''' ('''+" ,".join(parameters) + " ) values ( '" + "' ,'".join(values) + ''' ')'''
+        query = query + ''' ('''+" ,".join(parameters) + ") values (" + ",".join(delimiter) + ''') 
+        '''
         print("query", query)
-        isSuccessful = cursor.execute(query)
+        cursor.execute(query, tuple(values))
+        isSuccessful = cursor.rowcount
+    if isSuccessful and dataTableName == "credentials":
+        mailID = payload['input']['email']
+        message = '''Hi ''' + mailID + ''',\nGreetings from Next Stay, \nThis is your verification code {0}. \n\n\n Please don't share with others.'''.format(code)
+        # message = "This is your verification code" + str(code)
+        recipients = [mailID]
+        return {"message":message, "recipients":recipients}
     # fields = [field_md[0] for field_md in cursor.description]
     # result = [dict(zip(fields, row)) for row in cursor.fetchall()]
+    result = {"executedSuccessfully": isSuccessful}
+    print({"executedSuccessfully": isSuccessful})
+    conn.commit()
+    conn.close()
+    return result
+
+
+def update(payload):
+    conn = psycopg2.connect(
+        database="d1mcchg8iig4uf", user='qgopasstjgvbtx',
+        password='936bcf6d1c2d77bb52cac3b38baef32140407294a03b629c87a473cc5b5aa7d6',
+        host='ec2-52-22-136-117.compute-1.amazonaws.com', port='5432'
+    )
+
+    # Setting auto commit false
+    conn.autocommit = True
+
+    # Creating a cursor object using the cursor() method
+    parameters = payload['input'].keys()
+    values = payload['input'].values()
+    conditions = payload['query']
+    delimiter = []
+    for value in values:
+        delimiter.append("%s")
+    cursor = conn.cursor()
+    query = '''Update {0} set '''.format(payload['dataTable'])
+    for param in parameters:
+        query = query + ''' {0} = '{1}' ,'''.format(param, payload['input'][param])
+    query = query[:-1] + "where "
+    # query = query + ''' (''' + " ,".join(parameters) + ") values (" + ",".join(delimiter) + ''') '''
+    keys = conditions.keys()
+    for key in keys:
+        query = query + '''{0} = '{1}' AND'''.format(key, conditions[key])
+    query = query[:-4]
+    print("query", query)
+    cursor.execute(query, tuple(values))
+    isSuccessful = cursor.rowcount
     result = {"executedSuccessfully": isSuccessful}
     print({"executedSuccessfully": isSuccessful})
     conn.commit()
